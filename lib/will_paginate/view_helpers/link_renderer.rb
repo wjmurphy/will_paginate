871c989f8f355d8ef52849e5779107c930792e1e
wjmurphy <hello@wjmurphy.co.uk>
2020-11-13 22:06:33 +0000

    tweak:

--- lib/will_paginate/view_helpers/link_renderer.rb
+++ lib/will_paginate/view_helpers/link_renderer.rb
@@ -54,17 +54,17 @@ module WillPaginate
 
       def gap
         text = @template.will_paginate_translate(:page_gap) { '&hellip;' }
-        %(<li class="page-item">#{text}</li>)
+        %(<li class="page-item"><a href="# class="page-link disabled">#{text}</a></li>)
       end
 
       def previous_page
-        num = @collection.current_page > 1 && @collection.current_page - 1
-        previous_or_next_page(num, @options[:previous_label], 'page-item')
+        num = @collection.current_page
+        previous_or_next_page(num, @options[:previous_label], "page-item #{@collection.current_page > 1 && @collection.current_page - 1 ? "" : "disabled"}")
       end
 
       def next_page
-        num = @collection.current_page < total_pages && @collection.current_page + 1
-        previous_or_next_page(num, @options[:next_label], 'page-item')
+        num = @collection.current_page
+        previous_or_next_page(num, @options[:next_label], "page-item #{@collection.current_page < total_pages && @collection.current_page + 1 ? "" : "disabled"}")
       end
 
       def previous_or_next_page(page, text, classname)
